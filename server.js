9389689bf52da308beeeab0d3e9007e9aa2371d6
erdarafika <45282795+erdarafika@users.noreply.github.com>
2018-11-23 15:06:33 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+const bodyParser = require('body-parser')
+const app = express()
+const router = require('./router')
+const async = require('async')
+const config = require('./config/main')
+const SHA256 = require('crypto-js/sha256')
+const uuidv1 = require('uuid/v1')
+const port = 4000
+
+app.disable('x-powered-by')
+app.use(helmet())
+app.use(helmet.noCache())
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+})
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors())
+router(app)
+const server = app.listen(port)
